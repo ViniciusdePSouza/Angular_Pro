38157cbe79e259321d3724771705332a2907b990
ViniciusdePSouza <dev.souzavinicius@gmail.com>
2022-11-10 14:20:04 -0300

    FormArray

--- angular_pro/src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
+++ angular_pro/src/app/stock-inventory/components/stock-selector/stock-selector.component.ts
@@ -1,12 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
+import { Product } from '../../pages/models/product.interface'
+
 @Component({
   selector: 'stock-selector',
   templateUrl: './stock-selector.component.html',
   styleUrls: ['./stock-selector.component.scss']
 })
-export class StockSelectorComponent  {
+export class StockSelectorComponent {
+  
   @Input()
   parent!: FormGroup;
+
+  @Input()
+  products!: Product[];
 }
--- angular_pro/src/app/stock-inventory/pages/stock-inventory/stock-inventory.component.ts
+++ angular_pro/src/app/stock-inventory/pages/stock-inventory/stock-inventory.component.ts
@@ -2,25 +2,64 @@ import { Component } from '@angular/core';
 
 import { FormControl, FormGroup, FormArray } from '@angular/forms';
 
+import { Product } from '../../pages/models/product.interface'
+
 @Component({
   selector: 'stock-inventory',
   templateUrl: './stock-inventory.component.html',
   styleUrls: ['./stock-inventory.component.scss']
 })
 export class StockInventoryComponent {
+
+  products: Product[] = [
+    {
+      "id": 1,
+      "price": 2800,
+      "name": "McBook Pro"
+    },
+    {
+      "id":2,
+      "price": 50,
+      "name": "USB-C Adaptor"
+    },
+    {
+      "id": 3,
+      "price": 400,
+      "name": "iPod"
+    },
+    {
+      "id": 4,
+      "price": 900,
+      "name": "iPhone"
+    },
+    {
+      "id": 5,
+      "price": 600,
+      "name": "Apple Watch"
+    }
+  ]
+
   form = new FormGroup({
     store: new FormGroup({
       branch: new FormControl(''),
       code: new FormControl('')
     }),
 
-    selected: new FormGroup({
-      product_id: new FormControl(''),
-      quantity: new FormControl(10)
-    }),
+    selected: this.createStock({}),
 
-    stock: new FormArray([])
+    stock: new FormArray([
+      this.createStock({product_id: 1, quantity: 10}),
+      this.createStock({product_id: 2, quantity: 50}),
+      this.createStock({product_id: 3, quantity: 60})
+    ])
   })
+
+  createStock(stock: any) {
+    return new FormGroup({
+      product_id: new FormControl(stock.product_id || ''),
+      quantity: new FormControl(stock.quantity || 10)
+    })
+  }
 
    onSubmit() {
     console.log('Submit', this.form.value)
